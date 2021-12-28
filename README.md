4173a641e934820396ccb8d1b4b07433c3f33f7f
Dhruva Srinivas <dhruvas17068@gmail.com>
2021-12-28 16:49:04 +0530

    👋 hello world

---
+++ README.md
@@ -0,0 +1,7 @@
+# leetcode solutions
+
+This repo contains some of my solutions for [Leetcode](https://leetcode.com) problems. They're obviously not the best and its just kept for my personal reference :)
+
+# index
+
+[`1. Palindrome Number`](https://github.com/carrotfarmer/leetcode-solutions/blob/main/palindrome-number.ts)
---
+++ palindrome-number.ts
@@ -0,0 +1,8 @@
+// Palindrome Number
+// https://leetcode.com/problems/palindrome-number/submissions/
+// check if a number is palindrome (reversed value is the same as original value)
+
+function isPalindrome(x: number): boolean {
+  const y: string = x.toString().split("").reverse().join("");
+  return y === x.toString() ? true : false;
+}
